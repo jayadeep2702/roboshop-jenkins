aa5208c4eb1bf576d2cde7771584087554fc05a3
pavanivadaboina <Pavanivadaboina567@gmail.com>
2023-11-07 12:53:52 +0530

    terraform vpc code

---
+++ vars/terraform.groovy
@@ -0,0 +1,36 @@
+def call(int buildNumber) {
+    pipeline {
+        agent {
+            node {
+                label 'workstation'
+
+            }
+        }
+
+        parameters {
+            choice(name: 'env', choices: ['dev', 'prod'], description: 'pick environment')
+        }
+
+        stages {
+
+            stage('TERRAFORM INIT') {
+                steps {
+                    sh 'terraform init -backend-config=env-${env}/state.tfvars'
+                }
+            }
+
+            stage('TERRAFORM APPLY') {
+                steps {
+                    sh 'terraform apply -auto-approve -var-file=env-${env}/main.tf'
+                }
+            }
+        }
+
+
+        post {
+            always {
+                cleanWS()
+            }
+        }
+    }
+}
