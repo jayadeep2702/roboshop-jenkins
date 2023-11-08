33aec488bc63af09d6358c5e6f52fca7c3974260
pavanivadaboina <Pavanivadaboina567@gmail.com>
2023-11-08 14:15:22 +0530

    terraform vpc code

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -6,7 +6,9 @@ def call() {
 
             }
         }
-
+        options {
+            ansiColor('xterm')
+        }
         parameters {
             choice(name: 'env', choices: ['dev', 'prod'], description: 'pick environment')
         }
