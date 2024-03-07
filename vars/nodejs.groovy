86e2ed71fe8af444b21a0d29907c4a84b1f4d95b
pavanivadaboina <Pavanivadaboina567@gmail.com>
2024-03-07 11:18:54 +0530

    terraform vpc code

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -16,7 +16,7 @@ def call() {
 
             stage('code quality') {
                 steps {
-                     sh 'echo Code Quality'
+                     sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.43.197:9000 -Dsonar.login=admin -Dsonar.passwprd=admin123'
                 }
             }
             stage('Unit Test cases') {
