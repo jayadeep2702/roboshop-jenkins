6f559b5c24a8561af750fe8ef22f11341dfd710f
pavanivadaboina <Pavanivadaboina567@gmail.com>
2024-03-20 11:39:45 +0530

    terraform vpc code

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -44,7 +44,7 @@ def call() {
                 }
                 steps {
                     sh 'env'
-                    sh 'curl -v -u admin:admin123 --upload-file server.js https://172.31.12.50:8081/repository/cart/server.js'
+                    sh 'curl -v -u admin:admin123 --upload-file server.js http://172.31.12.50:8081/repository/cart/server.js'
                 }
             }
 
