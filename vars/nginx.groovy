0dc789a5f77766f5b5c2cab8269136f8aaacfd0f
akhil kumar <akhil@gmail.com>
2024-04-21 14:23:00 +0530

    moving files

--- vars/nginx.groovy
+++ vars/nginx.groovy
@@ -11,6 +11,10 @@ def call() {
             ansiColor('xterm')
         }
 
+        environment{
+            NEXUS= credentials('NEXUS')
+        }
+
 
         stages {
 
@@ -43,7 +47,6 @@ def call() {
                     }
                 }
                 steps {
-                    sh 'npm install'
                     sh 'echo $TAG_NAME >VERSION'
                     sh 'zip -r ${component}-${TAG_NAME}.zip *'
                     sh 'zip -d ${component}-${TAG_NAME}.zip jenkinsfile'
