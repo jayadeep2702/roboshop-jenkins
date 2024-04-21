1be865d575d1dbacedcac61a830de48d8c2daeb9
akhil kumar <akhil@gmail.com>
2024-04-21 14:35:28 +0530

    moving files

--- vars/nginx.groovy
+++ vars/nginx.groovy
@@ -50,7 +50,7 @@ def call() {
                     sh 'echo $TAG_NAME >VERSION'
                     sh 'zip -r ${component}-${TAG_NAME}.zip *'
                     sh 'zip -d ${component}-${TAG_NAME}.zip jenkinsfile'
-                    sh 'curl -v -u admin:admin123 --upload-file ${component}-${TAG_NAME}.zip http://172.31.12.50:8081/repository/${component}/${component}-${TAG_NAME}.zip'
+                    sh 'curl -f -v -u admin:admin123 --upload-file ${component}-${TAG_NAME}.zip http://172.31.12.50:8081/repository/${component}/${component}-${TAG_NAME}.zip'
                 }
             }
 
